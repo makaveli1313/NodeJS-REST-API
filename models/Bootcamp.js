ba534da49d7cde3bc129e36fd40e12495928933a
Marko Milovanov <markomilovanov@gmail.com>
2020-02-10 16:49:37 +0100

    added Geocoder and used on MongoDB GeoJSON Objects trough MongoDB middleware for getting Geo data out of typed address during bootcamp creation process

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
+
 const Schema = mongoose.Schema;
 
 const BootcampSchema = new Schema({
@@ -102,10 +104,27 @@ const BootcampSchema = new Schema({
 //Create bootcamp slug from the name
 
 BootcampSchema.pre('save', function(next) {
-  this.slug = slugify(this.name, {lower:true});
+  this.slug = slugify(this.name, { lower: true });
+
   next();
 });
 
+//Geocode & create location field
 
+BootcampSchema.pre('save', async function(next) {
+
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countrycode
+  };
+  next();
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
